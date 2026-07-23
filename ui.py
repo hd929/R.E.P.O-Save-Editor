19eb9eacbe95fc38d182a7ce76b5a556a5f7cdfb
Ajitana <handuy099@gmail.com>
2026-07-24 03:25:24 +0700

    chore: clean up repo, remove test files, spec files, unused UI concepts, update gitignore

--- ui.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logger = logging.getLogger(__name__)
-
-log_app = logging.getLogger('discord.app_commands')
-log_discord = logging.getLogger('discord')
-log = logging.getLogger('bot')
-
-log.warning("This is a warning message")
